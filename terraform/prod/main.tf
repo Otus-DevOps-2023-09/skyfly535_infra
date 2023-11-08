eaea4d15d795aff590552ad3725ad04dd96bd0f7
Roman Kozlovsky <skyfly534@gmail.com>
2023-11-08 22:05:25 +1000

    fix bag

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -15,7 +15,7 @@ provider "yandex" {
 }
 
 module "app" {
-  source              = "./modules/app"
+  source              = "../modules/app"
   public_key_path     = var.public_key_path
   private_key_path    = var.private_key_path
   app_disk_image      = var.app_disk_image
@@ -25,7 +25,7 @@ module "app" {
 }
 
 module "db" {
-  source              = "./modules/db"
+  source              = "../modules/db"
   public_key_path     = var.public_key_path
   private_key_path    = var.private_key_path
   db_disk_image       = var.db_disk_image
